8143c79fa1ce5b13a6c9f3eaba82177ae53eb851
Zülal Tak <54946626+zulaltak@users.noreply.github.com>
2022-10-04 11:41:35 +0300

    Create ForDemo.java
    Increment/Decrement Operators example

---
+++ Demos/ForDemo.java
@@ -0,0 +1,16 @@
+public class ForDemo {
+
+	public static void main(String[] args) {
+		// i+=2 --> i+2
+		for (int i=1;i<=10;i++) { 
+			System.out.println(i); // output : 1 2 3... 10
+		}
+		
+	
+		for (int i=2;i<=10;i+=2) { 
+			System.out.println(i); // output : 2 4 6 8 10
+		}
+	}
+	
+	
+}
