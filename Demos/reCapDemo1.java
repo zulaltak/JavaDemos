be6ac194cb738e2e2210e96cd876e1d6540dc95a
Zülal Tak <54946626+zulaltak@users.noreply.github.com>
2022-10-04 10:26:32 +0300

    Create reCapDemo1.java
    Find largest of three numbers

---
+++ Demos/reCapDemo1.java
@@ -0,0 +1,19 @@
+public class reCapDemo1 {
+
+	public static void main(String[] args) {
+		int n1=20;
+		int n2=25;
+		int n3=2;
+		
+		int largest = n1;
+		if (largest<n2) {
+				largest = n2;
+		}
+		if (largest<n3) {
+			largest= n3;
+		}
+		System.out.println("The largest number is: " +largest);
+
+	}
+
+}
